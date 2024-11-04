27a09a2203b2afd7a776e15cefc641c2ef6ec03c
Theresaa-K <kennytheresa7@gmail.com>
2024-11-03 21:30:13 -0600

    Project update

--- src/JavaProject/SaadiaProject.java
+++ src/JavaProject/SaadiaProject.java
@@ -1,10 +1,10 @@
 package JavaProject;
-
-public class SaadiaProject {
-    /*
+/*
 Create a 2D array or integer type where you will store odd and even numbers.
 Develop a program that will identify/print the even numbers only.
  */
+
+public class SaadiaProject {
     public static void main(String[] args) {
         int [][] numbers = {
                 {1,2,3},
