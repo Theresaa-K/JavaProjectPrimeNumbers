a471218af1ec22bb3207f57ca348acb26614c944
Theresaa-K <kennytheresa7@gmail.com>
2024-11-06 13:29:44 -0600

    Project update

---
+++ src/JavaProject/MilicaProject.java
@@ -0,0 +1,12 @@
+package JavaProject;
+
+public class MilicaProject {
+    public static void main(String[] args) {
+
+        int a = 5, b = 10;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
